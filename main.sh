2da79b891f9eff45dbc29b0e82923e7165840d4e
isongjosiah <isongjosiah@gmail.com>
2021-08-02 22:14:25 +0100

    Feat: Update bash file

--- main.sh
+++ main.sh
@@ -2,12 +2,47 @@
 # Bash script to run team mate scripts
 
 # clone the git hub repository
-git clone https://github.com/isongjosiah/team_pasteur.git
+# git clone https://github.com/isongjosiah/team_pasteur.git
 
 # move into the repo folder
 cd team_pasteur
 
 # list all files in the repo, loop through them and execute conditionally
-file=$(ls *)
-echo ${file}
+files=$(ls *)
+echo ${files}
 
+for file in $files
+do
+    # split files based on extension
+    filename=$(basename -- $file)
+    extension=${filename##*.}
+    filename=${filename%.*}
+
+    # execute based on extension
+    case $extension in
+    "c")
+        echo "C file"
+        ;;
+    "py")
+        echo "Python file"
+        ;;
+    "js")
+        echo "Javascript file"
+        ;;
+    "c++")
+        echo "C++ file"
+        ;;
+    "c#")
+        echo "C# file"
+        ;;
+    "java")
+        echo "Java file"
+        ;;
+    "r")
+        echo "R file"
+        ;;
+    "rb")
+        echo "Ruby file"
+    esac
+
+done
