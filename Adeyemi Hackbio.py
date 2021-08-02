ec526464ebe1468e727dbaf447ff8dfe049379fb
Adeyemisamm <88286477+Adeyemisamm@users.noreply.github.com>
2021-08-02 22:55:30 +0100

    Add files via upload

---
+++ Adeyemi Hackbio.py
@@ -0,0 +1,9 @@
+name= ("Samuel Adedayo")
+email= ("Samueladedayoyemi@gmail.com")
+slack= ("@Adeyemi") 
+biostack= ("Genomics")
+
+print("Name : " + name)
+print("Email : " + email)
+print("Slack Username : " + slack)
+print("Biostack : " + biostack)
