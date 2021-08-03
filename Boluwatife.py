edf49c988170fdaf774fad21483557c8590aeed3
ESTHER OLUNOIKI <88286274+Tife23@users.noreply.github.com>
2021-08-03 18:01:05 +0300

    Update and rename Boluwatife to Boluwatife.py

---
+++ Boluwatife.py
@@ -0,0 +1,4 @@
+name= Boluwatife Olunoiki, email= tifeolunoiki@gmail.com, slack= @Boluwatife, biostack= drug discovery and genomics
+
+print('name=Boluwatife Olunoiki,email=tifeolunoiki@gmail.com,slackusername=@Boluwatife,biostack=drug discovery and genomics')
+
