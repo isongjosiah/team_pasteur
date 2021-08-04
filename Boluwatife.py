12dacc67fce32d0f3012a710b7c6937e1c07bf18
ESTHER OLUNOIKI <88286274+Tife23@users.noreply.github.com>
2021-08-04 15:33:56 +0300

    Update Boluwatife.py

--- Boluwatife.py
+++ Boluwatife.py
@@ -1,4 +1,4 @@
- name=Boluwatife Olunoiki email=tifeolunoiki@gmail.com slackusername=@Boluwatife biostack=drug discovery and genomics
+ name=Boluwatife Olunoiki email=tifeolunoiki@gmail.com slackusername=@tifee_ twitter=@tifee_ humming distance= 0 biostack=drug discovery and genomics
 
-print('Boluwatife Olunoiki tifeolunoiki@gmail.com @Boluwatife drug discovery and genomics')
+print('Boluwatife Olunoiki tifeolunoiki@gmail.com @tifee_ @tifee_ 0 drug discovery and genomics')
 
