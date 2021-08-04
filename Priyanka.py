89a4a84139530bd2046a789f8e94878e902ac0dd
Priyanka Pandit <88287441+Piyupandit@users.noreply.github.com>
2021-08-04 16:34:43 +0530

    addition of twitter handle & hamming distance

--- Priyanka.py
+++ Priyanka.py
@@ -2,7 +2,22 @@
 
 name = 'Priyanka'
 email = 'priyupandit99@gmail.com'
-slack = '@Priyanka'
-Biostack = 'Drug Development'
+slack_username = '@PriyankaPandit'
+biostack = 'Drug Development'
+twitter_handle = '@PiiiyuuuPandit'
 
-print(name, email ,slack, Biostack )
+def h_d(string1, string2): 
+    # Start with a distance of zero, and count up
+    distance = 0
+    # Loop over the indices of the string
+    L = len(string1)
+    for i in range(L):
+        # Add 1 to the distance if these two characters are not equal
+        if string1[i] != string2[i]:
+            distance += 1
+    # Return the final count of differences
+    return distance
+
+hamming_distance = h_d(slack_username, twitter_handle)
+
+print(name, email ,slack_username, biostack, twitter_handle, hamming_distance )
