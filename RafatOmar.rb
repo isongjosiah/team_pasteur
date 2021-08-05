d0a3c7121860a22f004fb22cc2bb3d2911525c8d
Rafat0mar <73296870+Rafat0mar@users.noreply.github.com>
2021-08-05 14:59:16 +0100

    Update RafatOmar.rb

--- RafatOmar.rb
+++ RafatOmar.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 name = "Rafat Omar, "
 email = "rafat.0mar@outlook.com, "
-slack = "@RafatOmar,  "
+slack = "@RafatOmar, "
 biostack = "Genomics, "
 twitter='@rafat0mar, '
 
@@ -12,13 +12,17 @@ class Hamming
   # Computes the hamming distance between two strings.
     # Return <code>nil</code> for invalid parameters.
       def self.compute(word1, word2)
-      	return if (word1.nil? or word2.nil?)
+	      #checks if strings are empty
+      	return if (word1.empty? or word2.empty?)
+	      #turns words to lowercase
       	word1 = word1.downcase 
         word2 = word2.downcase
+	      #initialise count
       	hd   = 0
-	shortest = [word1, word2].min_by(&:length)
-
-	shortest.length.times do |index|
+	
+	#iterates through each letter and counts
+	# whenever there isnt a match
+	word1.length.times do |index|
 		if (word1[index] != word2[index])
 			hd += 1
 	 	end
