a4c872982cee80e5fe5c4d94ed83e665a48c400d
Ayoxd <88292598+Ayoxd@users.noreply.github.com>
2021-08-05 12:08:14 -0400

    Update Ayox.py

--- Ayox.py
+++ Ayox.py
@@ -2,3 +2,13 @@ Name= ('ogunlade ayodele')
 Email= ('ogunladeayodele.t@gmail.com')
 Slackname= ('Ayox')
 Biostack= ('Data Science')
+
+def hamming_distance(s1,s2):
+    count = 0
+
+    for i in range(len(s1)):
+        if s1[i] != s2[i]:
+            count += 1
+    return count
+
+print(hamming_distance('Ayox','yoxblog'))
