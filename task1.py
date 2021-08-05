9e2d985a10718fba674143a2cd4360937615573c
Shukurah Olajide <88286695+Bolleavean@users.noreply.github.com>
2021-08-05 22:18:00 +0100

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,4 +1,4 @@
 def task1():
-  print ('Name Olajide Shukurah Abolanle Email shukuraholajide@gmail.com slack username @AbolanleSh Biostack Genomics Twitter @Bolleavean Hammering distance 8')
+  print ('Name Olajide Shukurah Abolanle, Email shukuraholajide@gmail.com, slack username @AbolanleSh, Biostack Genomics, Twitter @Bolleavean, Hammering distance 8')
  
 task1()
