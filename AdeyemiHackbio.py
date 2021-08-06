150f40db52214706af9eb8708aa61ccf19ca7ea4
Adeyemisamm <88286477+Adeyemisamm@users.noreply.github.com>
2021-08-06 16:43:04 +0100

    Update AdeyemiHackbio.py

--- AdeyemiHackbio.py
+++ AdeyemiHackbio.py
@@ -6,18 +6,18 @@ slack_username = ('@Sadeyemi')
 biostack = ('Genomics')
 twitter_handle = ('@Yemisamx')
 
-def h_d(string1, string2):
+def h_d(str1, str2):
     # Start with a distance of zero, and count up
     distance = 0
     # Loop over the indices of the string
-    L = len(string1)
+    L = len(str1)
     for i in range(L):
         # Add 1 to the distance if these two characters are not equal
-        if string1[i] != string2[i]:
+        if str1[i] != str2[i]:
             distance += 1
     # Return the final count of differences
     return distance
 
 hamming_distance = h_d(slack_username, twitter_handle)
 
-print(name, email, slack_username, biostack, twitter_handle, hamming_distance)
+print(name, email,slack_username, biostack, twitter_handle, hamming_distance)
