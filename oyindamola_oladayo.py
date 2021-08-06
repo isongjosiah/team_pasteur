40d3182a343a4b8c67e2e31d17e40e69206640f4
oyin22 <88288139+oyin22@users.noreply.github.com>
2021-08-06 18:29:38 +0100

    Update oyindamola_oladayo.py

--- oyindamola_oladayo.py
+++ oyindamola_oladayo.py
@@ -1,1 +1,16 @@
-print("Oyindamola Oladayo, oyindamolaoladayo@gmail.com, @Oyin22, drug development ")
+def compute_hd(slack, twitter):
+    count = 0
+    max_length = 0
+
+    if (len(slack) > len(twitter)):
+        max_length = len(slack)
+    else:
+        max_length = len(twitter)
+
+    for i in range(max_length):
+        if (twitter[i] != slack[i]):
+            count += 1
+    return count
+
+
+print(f'Oyindamola oladayo, oyindamolaoladayo@gmail.com @oyin22 drug develpment, @oyin.o, {compute_hd("oyin22", "oyin.o")}')
