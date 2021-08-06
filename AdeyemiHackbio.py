ecb3b12d00c9cffa30494468f6d978c8db7d3ca4
Adeyemisamm <88286477+Adeyemisamm@users.noreply.github.com>
2021-08-06 16:24:37 +0100

    Update AdeyemiHackbio.py

--- AdeyemiHackbio.py
+++ AdeyemiHackbio.py
@@ -1,9 +1,23 @@
-name= ("Samuel Adedayo")
-email= ("Samueladedayoyemi@gmail.com")
-slack= ("@Adeyemi") 
-biostack= ("Genomics")
+#!/usr/bin/python3
 
-print("Name : " + name)
-print("Email : " + email)
-print("Slack Username : " + slack)
-print("Biostack : " + biostack)
+name = ('Samuel Adedayo')
+email = ('Samueladedayoyemi@gmail.com')
+slack_username = ('@Sadeyemi')
+biostack = ('Genomics')
+twitter_handle = ('@Yemisamx')
+
+def h_d(string1, string2):
+    # Start with a distance of zero, and count up
+    distance = 0
+    # Loop over the indices of the string
+    L = len(string1)
+    for i in range(L):
+        # Add 1 to the distance if these two characters are not equal
+        if string1[i] != string2[i]:
+            distance += 1
+    # Return the final count of differences
+    return distance
+
+hamming_distance = h_d(slack_username, twitter_handle)
+
+print(name, email, slack_username, biostack, twitter_handle, hamming_distance)
