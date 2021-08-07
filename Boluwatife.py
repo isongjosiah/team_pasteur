cb56e538432f9b9aadf17e59304121e970972106
isongjosiah <35474477+isongjosiah@users.noreply.github.com>
2021-08-07 11:02:46 +0100

    Update Boluwatife.py

--- Boluwatife.py
+++ Boluwatife.py
@@ -1,4 +1,1 @@
- name=Boluwatife Olunoiki,email=tifeolunoiki@gmail.com,slackusername=@tifee_,twitter=@tifee_,humming distance= 0,biostack=drug discovery and genomics
-
-print('Boluwatife Olunoiki,tifeolunoiki@gmail.com,@tifee_,@tifee_,0,drug discovery and genomics')
-
+print('Boluwatife Olunoiki,tifeolunoiki@gmail.com,@tifee_,drug discovery and genomics,@tifee_,0')
