072f211f07bcd31caaabb06615b0693f5e72732d
isongjosiah <isongjosiah@gmail.com>
2021-08-07 12:02:11 +0100

    Adjust biostack

--- josiah.c
+++ josiah.c
@@ -18,5 +18,5 @@ int calculate_hamming_dist() {
 }
 
 int main (int argc, char *argv[]) {
-    printf("Isong Josiah, isongjosiah@gmail.com, @%s,  Drug development, @%s, %d\n",slack, twitter, calculate_hamming_dist());
+    printf("Isong Josiah, isongjosiah@gmail.com, @%s,  Drug development and Genomics, @%s, %d\n",slack, twitter, calculate_hamming_dist());
 }
